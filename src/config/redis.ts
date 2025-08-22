50db21815fbdeec22800ec03575bce82259150cf
Mahmoud Elamrosy <74033364+melamrosy72@users.noreply.github.com>
2025-08-22 19:20:21 +0300

    monitoring redis connection  and error

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,8 +1,17 @@
+import 'dotenv/config'
 import { Redis } from "ioredis";
 
 const redis = new Redis({
-    host: "127.0.0.1",
-    port: 6379,
+    host: process.env.REDIS_HOST,
+    port: parseInt(process.env.REDIS_PORT || '6379'),
+});
+
+redis.on('connect', () => {
+    console.log('Connected to Redis');
+});
+
+redis.on('error', (err) => {
+    console.log(err.message);
 });
 
 export default redis;
